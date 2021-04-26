62726cb0904dff32e00df4329493228ab58ba4b1
wylie-u <wylieurig@gmail.com>
2021-04-26 17:51:54 -0600

    updating server file with mongoose connection

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const mongoose = require("mongoose");
 
 const path = require("path");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
